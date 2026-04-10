b682b31b0ee3d2dd19986eaef69c0b30cb8ec1fe
Aman <Lenevo@hotpriest.com>
2026-04-10 18:22:37 +0530

    computer  choice made

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-console.log("hello aman")
+const choice = ["ROCK", "PAPER", "SCISSOR"];
+const randomBufer = new  Uint32Array(1);
+window.crypto.getRandomValues(randomBufer);
+const randomNumber = randomBufer[0] / (0xFFFFFFFF + 1);
+const computerChoice = choice[Math.floor(randomNumber * choice.length)];
+console.log("Computer choice: " + computerChoice);
